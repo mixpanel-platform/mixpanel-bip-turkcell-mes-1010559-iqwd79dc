82371792e8f6db66d6eb5677a88481fe05782514
Bo <bowen.johnson@mixpanel.com>
2016-07-21 10:16:12 -0400

    Fixing header panel links
    make it so revenue links out properly

--- linkOuts.js
+++ linkOuts.js
@@ -27,7 +27,7 @@ $('.panel-footer-revenue').click(function () {
 	    //grab the top event that is generating revenue and store as a variable
 	    var revenueEvent = results[0][0].key[0]
 	    console.log(revenueEvent)
-	    var mp_url_platform =  'https://mixpanel.com/report/998157' //window.document.referrer.split('/mpplatform')[0]
+	    var mp_url_platform =  window.document.referrer.split('/mpplatform')[0]
 	    console.log(mp_url_platform)
 	    var mp_url_segmentation = mp_url_platform + "/segmentation/#action:sum,arb_event:"+revenueEvent+",bool_op:and,chart_analysis_type:linear,chart_type:line,from_date:-29,ms_checked:("+revenueEvent+":!t),ms_values:!("+revenueEvent+"),segfilter:!((dropdown_tab_index:0,property:(name:'$revenue',source:properties,type:number),selected_property_type:number,type:number)),segment_type:number,to_date:0,type:general,unit:day"
 	    window.open(mp_url_segmentation)
